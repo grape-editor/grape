92d70ade42c40f81e457fd65f6172d7ec3110932
Jonatas Teixeira <jonatas@jonatas-desktop.(none)>
2011-02-01 18:21:42 -0200

    Separete class of management interface

--- src/gui/MainScreen.ui.h
+++
@@ -1,5 +0,0 @@
-char *s = N_("Grape");
-char *s = N_("_Edit");
-char *s = N_("_File");
-char *s = N_("_Help");
-char *s = N_("_View");
--- src/language/translate.sh
+++ src/language/translate.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+rm ../gui/*.h
 intltool-extract --type=gettext/glade ../gui/*.ui
 xgettext --language=Python --keyword=_ --keyword=N_ --output=default.pot ../*.py ../gui/*.py ../gui/*.h ../lib/*.py
 msginit --input=default.pot --locale=pt_BR
