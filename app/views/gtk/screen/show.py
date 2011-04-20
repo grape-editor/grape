0c9fcfad02bdfc1141392c9d3bb47839f543f051
Jonatas Teixeira <jonatas.teixeira@gmail.com>
2011-04-20 00:46:51 -0300

    Vertical and horizontal align

--- app/views/gtk/screen/show.py
+++ app/views/gtk/screen/show.py
@@ -291,12 +291,36 @@ class ScreenShow(object):
         tab, i = self.current_tab()
         tab.zoom_default()
 
+    def menu_edit_horizontal_align(self, widget):
+        tab, page_number = self.current_tab()
+
+        if tab.graph.selected_vertices() < 2:
+            return False
+       
+        mean = int(sum(map(lambda x: x.position[1], tab.graph.selected_vertices())) / len(tab.graph.selected_vertices()))
+        for vertex in tab.graph.selected_vertices():
+            vertex.position[1] = mean
+
+        tab.queue_draw()                                    
+
+    def menu_edit_vertical_align(self, widget):
+        tab, page_number = self.current_tab()
+
+        if tab.graph.selected_vertices() < 2:
+            return False
+
+        mean = int(sum(map(lambda x: x.position[0], tab.graph.selected_vertices())) / len(tab.graph.selected_vertices()))
+
+        for vertex in tab.graph.selected_vertices():
+            vertex.position[0] = mean
+
+        tab.queue_draw()
+
     def menu_view_fullscreen(self, widget):
         if widget.get_active():
             self.screen.fullscreen()
         else:
             self.screen.unfullscreen()
-
 
     def menu_help_about(self, widget):
         AboutShow(self.builder)
