07b9b967d1e987f78fe6a03be5692c4860ea9c3a
Jonatas Teixeira <jonatas.teixeira@gmail.com>
2011-08-03 00:04:43 -0300

    Added function to set attributes on vertex

--- src/gui/edge.py
+++ src/gui/edge.py
@@ -30,58 +30,39 @@ class Edge(object):
         self.treeview_properties = self.builder.get_object("treeview_properties")
         self.treeview_properties.get_selection().set_mode(gtk.SELECTION_MULTIPLE)
 
+        self.treeview_create()
         self.init_general_fields()
-                
         self.builder.connect_signals(self)
         self.screen.show_all()
 
         self.liststore_properties = gtk.ListStore(str, str)
         self.treeview_properties.set_model(self.liststore_properties)
 		
+    def treeview_create(self):
+        renderer1 = gtk.CellRendererText()
+        renderer1.connect('edited', self.edit_properties, 0)
+        renderer1.set_property('editable', True)
+
+        renderer2 = gtk.CellRendererText()
+        renderer2.connect('edited', self.edit_properties, 1)
+        renderer2.set_property('editable', True)
+
+        self.treeview_properties.insert_column_with_attributes(-1, 'Identifier', renderer1, text=0)
+        self.treeview_properties.insert_column_with_attributes(-1, 'Value', renderer2, text=1)
+
     def switch_page(self, notebook, page, page_num):
         if page_num == 0:
             self.init_general_fields()
         elif page_num == 1:
-            self.init_property_fields()
-
-    def update_property(self, cell, path, new_text, column):
-        row_iter = self.liststore_properties.get_iter(path)
-    
-        identifier, value = self.liststore_properties.get(row_iter, 0, 1)
-
-        if column == 0:
-            # change attribue name
-            t_value = getattr(self.edge, "user_" + identifier)
-            delattr(self.edge, "user_" + identifier)
-            setattr(self.edge, "user_" + new_text, t_value)
-
-        elif column == 1:
-            # change attribute value
-            setattr(self.edge, "user_" + identifier, value)
-
-        self.liststore_properties.set_value(row_iter, column, new_text)
-
-        return
+            self.init_properties_fields()
 
     def init_general_fields(self):
-        renderer = gtk.CellRendererText()
-        renderer.connect('edited', self.update_property, 0)
-        renderer.set_property('editable', True)
-        self.treeview_properties.insert_column_with_attributes(-1, 'Identifier', renderer, text=0)
-
-        renderer = gtk.CellRendererText()
-        renderer.connect('edited', self.update_property, 1)
-        renderer.set_property('editable', True)
-        self.treeview_properties.insert_column_with_attributes(-1, 'Value', renderer, text=1)
-
         self.label_id.set_label(str(self.edge.id))
         self.text_title.set_text(self.edge.title)
-        
         self.color_edge.set_color(self.cairo_to_spin(self.edge.color))
-        
         self.adjustment_width.value = self.edge.width
 
-    def init_property_fields(self):
+    def init_properties_fields(self):
         self.liststore_properties.clear()
         for attr in self.edge.__dict__:
             if attr.startswith("user_"):
@@ -93,13 +74,13 @@ class Edge(object):
         key = event.keyval
 
         if key == gtk.keysyms.Delete:
-            self.remove_edge()
+            self.remove_properties()
             
         from gtk.gdk import CONTROL_MASK
 
         if (event.state & CONTROL_MASK):
             if key == gtk.keysyms.N or key == gtk.keysyms.n:
-                self.add_edge()
+                self.add_properties()
 
     def mouse_press(self, widget, event):
         path = widget.get_path_at_pos(int(event.x), int(event.y))
@@ -123,24 +104,24 @@ class Edge(object):
 
         if not self.menu:
             self.menu = gtk.Menu()
-            self.menu_add_property = gtk.MenuItem(_("_Add property"))
-            self.menu_remove_property = gtk.MenuItem(_("_Remove property"))
+            self.menu_add_properties = gtk.MenuItem(_("_Add properties"))
+            self.menu_remove_properties = gtk.MenuItem(_("_Remove properties"))
 
-            self.menu_add_property.connect("activate", execute_action, self.add_property)
-            self.menu_remove_property.connect("activate", execute_action, self.remove_property)
+            self.menu_add_properties.connect("activate", execute_action, self.add_properties)
+            self.menu_remove_properties.connect("activate", execute_action, self.remove_properties)
 
-            self.menu.append(self.menu_add_property)
-            self.menu.append(self.menu_remove_property)
+            self.menu.append(self.menu_add_properties)
+            self.menu.append(self.menu_remove_properties)
             
         if selection.count_selected_rows() > 0:
-            self.menu_remove_property.set_sensitive(True)
+            self.menu_remove_properties.set_sensitive(True)
         else:
-            self.menu_remove_property.set_sensitive(False)
+            self.menu_remove_properties.set_sensitive(False)
 
         self.menu.show_all()
         self.menu.popup(None, None, None, event.button, event.time)
 
-    def add_property(self):
+    def add_properties(self):
         t_identifier = "foo"
         t_value = "bar"
         self.liststore_properties.append([t_identifier, t_value])
@@ -150,7 +131,7 @@ class Edge(object):
 #        self.add_state()
 #        self.area.queue_draw()
 
-    def remove_property(self):
+    def remove_properties(self):
         selection = self.treeview_properties.get_selection()
         store, rows = selection.get_selected_rows()
         
@@ -159,17 +140,36 @@ class Edge(object):
 
         for row in rows_reference:
             row_iter = store.get_iter(row.get_path())
-            property_identifier = store.get_value(row_iter, 0)
+            properties_identifier = store.get_value(row_iter, 0)
             store.remove(row_iter)
 
             for attr in self.edge.__dict__:
                 if attr.startswith("user_"):
                     t_identifier = attr[5:]
-                    if t_identifier == property_identifier:
+                    if t_identifier == properties_identifier:
                         delattr(self.edge, attr)
     
 #        self.add_state()
 #        self.area.queue_draw()
+
+    def edit_properties(self, cell, path, new_text, column):
+        row_iter = self.liststore_properties.get_iter(path)
+    
+        identifier, value = self.liststore_properties.get(row_iter, 0, 1)
+
+        if column == 0:
+            # change attribue name
+            t_value = getattr(self.edge, "user_" + identifier)
+            delattr(self.edge, "user_" + identifier)
+            setattr(self.edge, "user_" + new_text, t_value)
+
+        elif column == 1:
+            # change attribute value
+            setattr(self.edge, "user_" + identifier, value)
+
+        self.liststore_properties.set_value(row_iter, column, new_text)
+
+        return
 
     def cairo_to_spin(self, color):
         return gtk.gdk.Color(color[0] * 65535, color[1] * 65535, color[2] * 65535)
--- src/gui/vertex.py
+++ src/gui/vertex.py
@@ -20,7 +20,8 @@ class Vertex(object):
         self.add_state = graph_show.add_state
         self.builder = graph_show.builder
 
-        self.menu = None
+        self.menu_edges = None
+        self.menu_properties = None
 
         self.builder.add_from_file(path)
 
@@ -33,22 +34,43 @@ class Vertex(object):
         self.color_border = self.builder.get_object("color_border")
         self.adjustment_radius = self.builder.get_object("adjustment_radius")
         self.adjustment_border = self.builder.get_object("adjustment_border")
-
+        self.notebook = self.builder.get_object("notebook")
         self.treeview_edges = self.builder.get_object("treeview_edges")
-        self.treeview_edges.get_selection().set_mode(gtk.SELECTION_MULTIPLE)
-        
-        self.liststore_edges = self.builder.get_object("liststore_edges")
+        self.treeview_properties = self.builder.get_object("treeview_properties")
 
-        self.init_general_fields()
-                
+        self.treeview_edges.get_selection().set_mode(gtk.SELECTION_MULTIPLE)                
+        self.treeview_properties.get_selection().set_mode(gtk.SELECTION_MULTIPLE)
+
+        self.liststore_edges = gtk.ListStore(str, str, str)
+        self.liststore_properties = gtk.ListStore(str, str)
+
+        self.treeview_edges.set_model(self.liststore_edges)
+        self.treeview_properties.set_model(self.liststore_properties)
+
+        self.treeview_create()
         self.builder.connect_signals(self)
+        self.init_general_fields()
         self.screen.show_all()
+
+    def treeview_create(self):
+        renderer1 = gtk.CellRendererText()
+        renderer1.connect('edited', self.edit_properties, 0)
+        renderer1.set_property('editable', True)
+
+        renderer2 = gtk.CellRendererText()
+        renderer2.connect('edited', self.edit_properties, 1)
+        renderer2.set_property('editable', True)
+
+        self.treeview_properties.insert_column_with_attributes(-1, 'Identifier', renderer1, text=0)
+        self.treeview_properties.insert_column_with_attributes(-1, 'Value', renderer2, text=1)
 
     def switch_page(self, notebook, page, page_num):
         if page_num == 0:
             self.init_general_fields()
         elif page_num == 1:
             self.init_edges_fields()
+        elif page_num == 2:
+            self.init_properties_fields()
 
     def init_general_fields(self):
         self.label_id.set_label(str(self.vertex.id))
@@ -65,26 +87,51 @@ class Vertex(object):
     def init_edges_fields(self):
         number_of_edges = len(self.vertex.adjacencies)
         self.liststore_edges.clear()
-       
+
         for i in range(number_of_edges):
             t_id = self.vertex.adjacencies[i].id
             t_start = self.vertex.adjacencies[i].start.title
             t_end = self.vertex.adjacencies[i].end.title
             self.liststore_edges.append([t_id, t_start, t_end])
 
+    def init_properties_fields(self):
+        self.liststore_properties.clear()
+        for attr in self.vertex.__dict__:
+            if attr.startswith("user_"):
+                t_identifier = attr[5:]
+                t_value = getattr(self.vertex, attr)
+                self.liststore_properties.append([t_identifier, t_value])
+
     def keyboard_press(self, widget, event):
+        current_page_number = self.notebook.get_current_page()
         key = event.keyval
 
-        if key == gtk.keysyms.Delete:
-            self.remove_edge()
-            
-        from gtk.gdk import CONTROL_MASK
+        # Get like pointer to function
+        add = remove = None
+        if current_page_number == 1:
+            add = self.add_edge
+            remove = self.remove_edge
+        elif current_page_number == 2:
+            add = self.add_properties
+            remove = self.remove_properties
 
+        from gtk.gdk import CONTROL_MASK
+        if key == gtk.keysyms.Delete:
+            remove()
         if (event.state & CONTROL_MASK):
             if key == gtk.keysyms.N or key == gtk.keysyms.n:
-                self.add_edge()
+                add()
 
     def mouse_press(self, widget, event):
+        current_page_number = self.notebook.get_current_page()
+
+        # Get like pointer to function
+        right_click_menu = None
+        if current_page_number == 1:
+            right_click_menu = self.right_click_menu_edges
+        elif current_page_number == 2:
+            right_click_menu = self.right_click_menu_properties
+
         path = widget.get_path_at_pos(int(event.x), int(event.y))
         selection = widget.get_selection()
         
@@ -95,17 +142,16 @@ class Vertex(object):
         if event.button == 3:
             if path:
                 selection.select_path(path[0])
-            self.right_click_menu(event)
-           
+            right_click_menu(event)
     
-    def right_click_menu(self, event):
+    def right_click_menu_edges(self, event):
         selection = self.treeview_edges.get_selection()
 
         def execute_action(event, action):
             action()
 
-        if not self.menu:
-            self.menu = gtk.Menu()
+        if not self.menu_edges:
+            self.menu_edges = gtk.Menu()
             self.menu_add_edge = gtk.MenuItem(_("_Add edge"))
             self.menu_remove_edge = gtk.MenuItem(_("_Remove edge"))
             self.menu_edit_edge = gtk.MenuItem(_("_Edit edge settings"))
@@ -114,9 +160,9 @@ class Vertex(object):
             self.menu_remove_edge.connect("activate", execute_action, self.remove_edge)
             self.menu_edit_edge.connect("activate", execute_action, self.edit_edge)
 
-            self.menu.append(self.menu_add_edge)
-            self.menu.append(self.menu_remove_edge)
-            self.menu.append(self.menu_edit_edge)
+            self.menu_edges.append(self.menu_add_edge)
+            self.menu_edges.append(self.menu_remove_edge)
+            self.menu_edges.append(self.menu_edit_edge)
 
         if len(self.graph.vertices) > 1:
             self.menu_add_edge.set_sensitive(True)
@@ -133,8 +179,33 @@ class Vertex(object):
             self.menu_remove_edge.set_sensitive(False)
             self.menu_edit_edge.set_sensitive(False)
 
-        self.menu.show_all()
-        self.menu.popup(None, None, None, event.button, event.time)
+        self.menu_edges.show_all()
+        self.menu_edges.popup(None, None, None, event.button, event.time)
+
+    def right_click_menu_properties(self, event):
+        selection = self.treeview_properties.get_selection()
+
+        def execute_action(event, action):
+            action()
+
+        if not self.menu_properties:
+            self.menu_properties = gtk.Menu()
+            self.menu_add_properties = gtk.MenuItem(_("_Add properties"))
+            self.menu_remove_properties = gtk.MenuItem(_("_Remove properties"))
+
+            self.menu_add_properties.connect("activate", execute_action, self.add_properties)
+            self.menu_remove_properties.connect("activate", execute_action, self.remove_properties)
+
+            self.menu_properties.append(self.menu_add_properties)
+            self.menu_properties.append(self.menu_remove_properties)
+            
+        if selection.count_selected_rows() > 0:
+            self.menu_remove_properties.set_sensitive(True)
+        else:
+            self.menu_remove_properties.set_sensitive(False)
+
+        self.menu_properties.show_all()
+        self.menu_properties.popup(None, None, None, event.button, event.time)
 
     def edit_edge(self):
         selection = self.treeview_edges.get_selection()
@@ -185,6 +256,56 @@ class Vertex(object):
     
         self.add_state()
         self.area.queue_draw()
+
+    def edit_properties(self, cell, path, new_text, column):
+        row_iter = self.liststore_properties.get_iter(path)
+    
+        identifier, value = self.liststore_properties.get(row_iter, 0, 1)
+
+        if column == 0:
+            # change attribue name
+            t_value = getattr(self.vertex, "user_" + identifier)
+            delattr(self.vertex, "user_" + identifier)
+            setattr(self.vertex, "user_" + new_text, t_value)
+
+        elif column == 1:
+            # change attribute value
+            setattr(self.vertex, "user_" + identifier, value)
+
+        self.liststore_properties.set_value(row_iter, column, new_text)
+        return
+
+    def add_properties(self):
+        t_identifier = "foo"
+        t_value = "bar"
+        self.liststore_properties.append([t_identifier, t_value])
+
+        setattr(self.vertex, "user_" + t_identifier, t_value)
+        
+#        self.add_state()
+#        self.area.queue_draw()
+
+    def remove_properties(self):
+        selection = self.treeview_properties.get_selection()
+        store, rows = selection.get_selected_rows()
+        
+        #Scamp way to delete all selecteds rows =D
+        rows_reference = [gtk.TreeRowReference(store, row) for row in rows]
+
+        for row in rows_reference:
+            row_iter = store.get_iter(row.get_path())
+            properties_identifier = store.get_value(row_iter, 0)
+            store.remove(row_iter)
+
+            for attr in self.vertex.__dict__:
+                if attr.startswith("user_"):
+                    t_identifier = attr[5:]
+                    if t_identifier == properties_identifier:
+                        delattr(self.vertex, attr)
+                        break
+
+#        self.add_state()
+#        self.area.queue_draw()
 
     def cairo_to_spin(self, color):
         return gtk.gdk.Color(color[0] * 65535, color[1] * 65535, color[2] * 65535)
