1408d1d3d02cc55cd976e423a9edd6a666f2370a
Jonatas <jonatas.teixeira@gmail.com>
2011-09-15 09:58:48 -0300

    Just a litle sintaxe fix

--- src/gui/area.py
+++ src/gui/area.py
@@ -137,7 +137,7 @@ class GraphArea(DrawingArea):
                     cairo.curve_to(x3, y3, x4, y4, x2, y2)
                     cairo.stroke()
                     
-                    if not edge.bidirectional:
+                    if  edge.directed:
                         self.draw_arrow(cairo, (x4, y4), (x2, y2))
                         
                     before = edge
